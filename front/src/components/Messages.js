883234faf28479e73e00ef9ddb13ed94fdbc8e58
Vatsal Sharma <vatsalsharma376@gmail.com>
2020-10-12 22:30:18 +0530

    Update Messages.js

--- front/src/components/Messages.js
+++ front/src/components/Messages.js
@@ -6,6 +6,7 @@ const MessageDiv = styled.div`
   // Make it easy to visually differentiate messages
   padding-bottom: 7px;
   overflow: scroll;
+  min-height: min-content;
   height: 100%;
 `
 
@@ -23,6 +24,7 @@ const MessagesDiv = styled.div`
   height: 100%;
   padding: 1rem;
   overflow: hidden;
+  min-height: min-content;
   color: ${colors.layer1Foreground};
   // Copied from default React index.css
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
